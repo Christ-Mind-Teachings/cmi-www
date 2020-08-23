42df6d892036449e0633380857540d8f76f0547a
Rick Mercer <rmercer33@gmail.com>
2020-08-23 10:52:29 -1000

    generated files from last build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require("webpack");
 //const etp = require("extract-text-webpack-plugin");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+//const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 //const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
