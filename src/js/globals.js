389dd20ce7a997ea7569b815c61c82dc86ba8f77
Rick Mercer <rmercer33@gmail.com>
2022-10-26 15:25:45 -0400

    using acol.enable@christmind.info as address to request full acol access

--- src/js/globals.js
+++ src/js/globals.js
@@ -3,7 +3,7 @@
 */
 
 export default {
-  acolManager: "rmercer33+acol@gmail.com",
+  acolManager: "acol.enable@christmind.info",
   cmiUserId: "05399539cca9ac38db6db36f5c770ff1",
   sources: "/public/config/sources.json",
   user: "https://kkdlxunoe7.execute-api.us-east-1.amazonaws.com/latest",
